52b038fccf141654c9f656c2786f60b07729c473
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-08-26 00:21:58 -0700

    dirty

--- backend/discord/src/main.rs
+++ backend/discord/src/main.rs
@@ -11,6 +11,7 @@ use serenity::{
     prelude::{Context, EventHandler, GatewayIntents},
     Client,
 };
+use tokio::sync::OnceCell;
 
 #[group]
 #[commands(
@@ -29,8 +30,14 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {}
 
+static GAMESTATE: OnceCell<game_state::GameState> = OnceCell::const_new();
+
 #[tokio::main]
 async fn main() {
+    GAMESTATE
+        .set(game_state::GameState::new())
+        .expect("Failed to initialize GAMESTATE");
+
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!"))
         .group(&CODEPARTY_GROUP);
@@ -97,4 +104,50 @@ async fn forfeit(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id.say(&ctx.http, "Pong!").await?;
 
     Ok(())
+}
+
+mod game_state {
+    use std::{borrow::Cow, sync::Arc};
+
+    use dashmap::DashMap;
+    use uuid_b64::UuidB64;
+
+    #[derive(Clone, Debug)]
+    pub(crate) struct GameState {
+        room_map: Arc<DashMap<UuidB64, Room>>, // Maps room id to room
+        player_map: Arc<DashMap<Cow<'static, str>, UuidB64>>, // Checks if user is already in a game
+    }
+    impl GameState {
+        pub(crate) fn new() -> Self {
+            Self {
+                room_map: Arc::new(DashMap::new()),
+                player_map: Arc::new(DashMap::new()),
+            }
+        }
+
+        async fn create_party(&self, creator: Player, n_problems: usize) {
+            let room_id = UuidB64::new();
+            self.room_map
+                .insert(room_id, Room::new(creator, n_problems));
+        }
+    }
+
+    #[derive(Debug)]
+    struct Room {
+        players: Vec<Player>,
+        n_problems: usize,
+        problems_played: usize,
+    }
+    impl Room {
+        fn new(creator: Player, n_problems: usize) -> Self {
+            Self {
+                players: Vec::new(),
+                n_problems,
+                problems_played: 0,
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    struct Player {}
 }
